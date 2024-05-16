33b6b3d0f49abbe075b1e554c81670674b212ba5
maria-balwi <patricereyes.MPR@gmail.com>
2024-05-16 13:59:44 +0800

    added config and init file, fixed file structure

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    //DATABASE CONFIGURATIONS
+    $host = "localhost";
+    $username = "celobusi_payroll_system_user";
+    $password = "C3l0p@ssw0rd@65";
+    $database = "celobusi_payroll_system";
+
+    //API Keys
+
+?>  
---
+++ init.php
@@ -0,0 +1,20 @@
+<?php
+
+    // INCLUDE THE CONFIGURATION FILE
+    include 'config/config.php';
+
+    // DEFINE CONSTANTS FOR DATABASE CONFIGURATION
+    define('HOST', $host);
+    define('USER', $username);
+    define('PASSWORD', $password);
+    define('DATABASE', $database);
+
+    // require 'backend/Database.php';
+    // require 'backend/Users.php';
+    // require 'backend/Tickets.php';
+
+    // $database = new Database;
+    // $users = new Users;
+    // $tickets = new Tickets;
+
+?>
