c8072d760127b2e543bf41c7a84538635f7f8485
maria-balwi <patricereyes.MPR@gmail.com>
2024-05-16 16:28:04 +0800

    made changes to package.json for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./*.php', './**/*.php'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
