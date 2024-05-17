44ac6b10de7858b7fe0245f28fedeb18a5108b46
maria-balwi <patricereyes.MPR@gmail.com>
2024-05-17 17:24:54 +0800

    created sample dashboard page with sidebar for modules and navbar for profile section and logout

---
+++ pages/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- HEADER -->
+        <?php include('../includes/header.php'); ?>
+    </head>
+    <body>
+        <!-- SIDEBAR -->
+        <?php include('../includes/sidebar.php'); ?>	
+ 
+            <!-- MAIN CONTENT -->
+            <div class="flex-1 p-4 text-2xl font-bold overflow-auto">
+                Main Content Area
+            </div>
+            
+        </div>
+    
+        <!-- FOOTER -->
+        <?php include('../includes/footer.php'); ?>
+    </body>
+</html>
