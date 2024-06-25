b990761079b0271f07421aa8568bebf79652e627
maria-balwi <patricereyes.MPR@gmail.com>
2024-06-25 12:59:37 +0800

    added php file for functions and classes

---
+++ backend/Users.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Users extends Database
+    {
+        private $employees = 'tbl_employee';
+        private $users = 'tbl_users';
+        private $department = 'tbl_department';
+        private $designation = 'tbl_designation';
+        private $dbConnect = false;
+        public function __construct() {
+            $this->dbConnect = $this->dbConnect();
+        }
+
+        public function isLoggedIn() {
+            if ($_SESSION["logged_in"] == TRUE) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+?>
