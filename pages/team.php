4fb2b3be0fc213df49ee401267d2814618b85a02
maria-balwi <patricereyes.MPR@gmail.com>
2024-06-26 12:10:55 +0800

    created function for viewing all team in teams module

--- pages/team.php
+++ pages/team.php
@@ -20,8 +20,8 @@
             <div class="2xl:max-w-2xl p-4 m-1 bg-white border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700">
                 
                 <!-- DATATABLE -->
-                <div class="container mx-auto mt-5 overflow-auto">
-                    <table id="teamTable" class="min-w-full divide-y divide-gray-200">
+                <div class="container mx-auto overflow-auto">
+                    <table id="teamTable" class="min-w-full divide-y divide-gray-200 table-striped table-bordered">
                         <thead class="bg-gray-50">
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
@@ -31,30 +31,25 @@
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">Christian Jay Manahan Palencia</td>
-                                <td class="px-6 py-4 whitespace-nowrap">christian.palencia@celoph.com</td>
-                                <td class="px-6 py-4 whitespace-nowrap">(926) 027-2520</td>
-                                <td class="px-6 py-4 whitespace-nowrap">IT</td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">John Paul Peliazar Sampang</td>
-                                <td class="px-6 py-4 whitespace-nowrap">johnpaul.sampang@celoph.com</td>
-                                <td class="px-6 py-4 whitespace-nowrap">(918) 244-8396</td>
-                                <td class="px-6 py-4 whitespace-nowrap">IT</td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">Joseph Paul Gavino Odulio</td>
-                                <td class="px-6 py-4 whitespace-nowrap">josephpaul.odulio@celoph.com</td>
-                                <td class="px-6 py-4 whitespace-nowrap">(906) 270-3926</td>
-                                <td class="px-6 py-4 whitespace-nowrap">IT</td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">Maria Patrice Alvarez Reyes</td>
-                                <td class="px-6 py-4 whitespace-nowrap">patrice.reyes@celoph.com</td>
-                                <td class="px-6 py-4 whitespace-nowrap">(991) 657-7916</td>
-                                <td class="px-6 py-4 whitespace-nowrap">IT</td>
-                            </tr>
+                            <?php
+                                $teamQuery = mysqli_query($conn, $employees->viewTeam());
+                                while ($teamDetails = mysqli_fetch_array($teamQuery)) {
+
+                                    $team_id = $teamDetails['id'];
+                                    $team_employeeName = $teamDetails['employeeName'];
+                                    $team_emailAddress = $teamDetails['emailAddress'];
+                                    $team_mobileNumber = $teamDetails['mobileNumber'];
+                                    $team_department = $teamDetails['departmentName'];
+
+
+                                    echo "<tr data-id='" . $team_id . "' class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>";
+                                    echo "<td ='px-6 py-4 whitespace-nowrap'>" . $team_employeeName . "</td>";
+                                    echo "<td ='px-6 py-4 whitespace-nowrap'>" . $team_emailAddress . "</td>";
+                                    echo "<td ='px-6 py-4 whitespace-nowrap'>" . $team_mobileNumber . "</td>";
+                                    echo "<td ='px-6 py-4 whitespace-nowrap'>" . $team_department . "</td>";
+                                    echo "</td>";
+                                }
+                            ?>
                         </tbody>
                     </table>
                 </div>
