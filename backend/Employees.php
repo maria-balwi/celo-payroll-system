ca0f27678aec9a66a16cbe0bc6e51444e851e0cd
maria-balwi <patricereyes.MPR@gmail.com>
2024-06-27 11:18:11 +0800

    added tables and data on user dtr

--- backend/Employees.php
+++ backend/Employees.php
@@ -6,6 +6,9 @@
         // private $users = 'tbl_users';
         private $department = 'tbl_department';
         // private $designation = 'tbl_designation';
+        private $attendance = 'tbl_attendance';
+        private $logtype = 'tbl_logtype';
+        private $shift = 'tbl_shiftschedule';
         private $dbConnect = false;
         public function __construct() {
             $this->dbConnect = $this->dbConnect();
@@ -17,6 +20,15 @@
                 INNER JOIN ".$this->department." AS department
                 ON employees.departmentID = department.departmentID";
             return $team;
+        }
+
+        public function viewDTR($id) {
+            $dtr = "
+                SELECT * FROM ".$this->attendance." AS attendance INNER JOIN ".$this->employees." AS employees
+                ON attendance.empID = employees.id
+                INNER JOIN ".$this->logtype." AS logtype ON attendance.logTypeID = logtype.logTypeID INNER JOIN ".$this->shift." AS shift ON employees.shiftID = shift.shiftID
+                WHERE empID='$id'";
+            return $dtr;
         }
 
     }
--- pages/user_dtr.php
+++ pages/user_dtr.php
@@ -18,28 +18,20 @@
                 <div class="2xl:max-w-2xl p-4 m-0 bg-white border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700">
 
                     <!-- DATATABLE -->
-                    <div class="container mx-auto mt-5 overflow-auto">
-                        <table id="example" class="min-w-full divide-y divide-gray-200">
+                    <div class="container mx-auto overflow-auto">
+                        <table id="example" class="min-w-full divide-y divide-gray-200 text-center table-striped table-bordered">
                             <thead class="bg-gray-50">
                                 <tr>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Face DTR</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Day</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Shuft</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Time In</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Start of Break</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">End of Break</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Time Out</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Late</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">UT</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">RMW</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total OT</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total ND</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total NDOT</th>
+                                    <th class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Face DTR</th>
+                                    <th class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
+                                    <th class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Shift</th>
+                                    <th class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Attendance Type</th>
+                                    <th class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Time</th>
+                                    
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
-                                <tr>
+                                <!-- <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <svg class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
@@ -59,91 +51,48 @@
                                     <td class="px-6 py-4 whitespace-nowrap">18</td>
                                     <td class="px-6 py-4 whitespace-nowrap"></td>
                                     <td class="px-6 py-4 whitespace-nowrap"></td>
-                                </tr> 
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <svg class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                        </svg>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">Tue</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">05/21/2024</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">9am-6pm</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">8:42 AM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">6:05 PM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">480</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">5</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                </tr> 
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <svg class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                        </svg>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">Wed</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">05/22/2024</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">9am-6pm</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">8:44 AM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">6:03 PM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">480</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">3</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                </tr> 
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <svg class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                        </svg>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">Thu</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">05/23/2024</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">9am-6pm</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">8:19 AM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">6:03 PM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">8</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">480</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">3</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                </tr> 
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <svg class="h-8 w-8 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                        </svg>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">Mon</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">05/24/2024</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">9am-6pm</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">8:27 AM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">6:05 PM</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap">480</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">5</td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                    <td class="px-6 py-4 whitespace-nowrap"></td>
-                                </tr> 
+                                </tr>  -->
+                                <?php
+                                    $dtrQuery = mysqli_query($conn, $employees->viewDTR($_SESSION['id']));
+                                    while ($dtrDetails = mysqli_fetch_array($dtrQuery)) {
+
+                                        $dtr_id = $dtrDetails['id'];
+                                        $dtr_attendanceDate = $dtrDetails['attendanceDate'];
+                                        $dtr_attendanceTime = $dtrDetails['attendanceTime'];
+                                        $dtr_logType = $dtrDetails['logType'];
+                                        $dtr_time = $dtrDetails['time'];
+
+                                        $dateTime = new DateTime($dtr_attendanceDate);
+                                        $formattedDate = $dateTime->format('M d, Y');                                      
+
+                                        echo "<tr data-id='" . $dtr_id . "' class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>";
+                                        echo "<td class='px-6 py-4 whitespace-nowrap'>
+                                                <svg class='h-8 w-8 text-gray-500'  fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                                                    <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z'/>
+                                                    <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 13a3 3 0 11-6 0 3 3 0 016 0z'/>
+                                                </svg>
+                                            </td>";
+                                        echo "<td ='px-6 py-4 whitespace-nowrap'>" . $formattedDate . "</td>";
+                                        echo "<td ='px-6 py-4 whitespace-nowrap'>" . $dtr_time . "</td>";
+                                        if ($dtr_logType == "Time In") {
+                                            echo "<td><p class='inline-block bg-green-500 text-white px-3 py-1 rounded-full text-sm'>". $dtr_logType . "</p></td>";
+                                        }
+                                        else if ($dtr_logType == "Time Out" || $dtr_logType == "OT Out") {
+                                            echo "<td><p class='inline-block bg-red-500 text-white px-3 py-1 rounded-full text-sm'>". $dtr_logType . "</p></td>";
+                                        }
+                                        else if ($dtr_logType == "Start of Break" || $dtr_logType == "End of Break") {
+                                            echo "<td><p class='inline-block bg-purple-500 text-white px-3 py-1 rounded-full text-sm'>". $dtr_logType . "</p></td>";
+                                        }
+                                        else if ($dtr_logType == "OT In") {
+                                            echo "<td><p class='inline-block bg-blue-500 text-white px-3 py-1 rounded-full text-sm'>". $dtr_logType . "</p></td>";
+                                        }
+                                        else if ($dtr_logType == "Late" || $dtr_logType == "Undertime") {
+                                            echo "<td><p class='inline-block bg-yellow-500 text-white px-3 py-1 rounded-full text-sm'>". $dtr_logType . "</p></td>";
+                                        }
+                                        echo "<td ='px-6 py-4 whitespace-nowrap'>" . $dtr_attendanceTime . "</td>";
+                                        echo "</td>";
+                                    }
+                                ?>
                             </tbody>
                         </table>
                     </div>
