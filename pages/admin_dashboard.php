7ec66e8172bee921f170e24c749cbd2b05b0fe62
maria balwi <patricereyes.MPR@gmail.com>
2024-07-24 17:18:32 +0800

    updated svg icon on admin's dashboard module

--- pages/admin_dashboard.php
+++ pages/admin_dashboard.php
@@ -79,8 +79,11 @@
 
                     <!-- Card 5 -->
                     <div class="bg-white p-4 rounded-lg col-span-3 lg:col-span-1 shadow-md text-center">
-                        <svg class="h-16 w-16 text-gray-600 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <!-- <svg class="h-16 w-16 text-gray-600 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                        </svg> -->
+                        <svg class="h-16 w-16 text-gray-600 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
                         <h2 class="text-xl font-bold mb-1">
                             <?php  
