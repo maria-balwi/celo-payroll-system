b980a48a169b1502edc7abc5e4d4e160baab060e
maria balwi <patricereyes.MPR@gmail.com>
2024-07-24 17:22:53 +0800

    updated svg icon for leave applications on admin's dashboard module

--- pages/admin_dashboard.php
+++ pages/admin_dashboard.php
@@ -112,7 +112,7 @@
                                     <div class="flex gap-2 p-2 rounded-lg hover:bg-blue-100 px-auto">
                                         <div class="my-auto">
                                             <svg class="h-10 w-10 text-gray-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"/>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                                             </svg>
                                         </div>
                                         <div class="py-auto px-auto">
