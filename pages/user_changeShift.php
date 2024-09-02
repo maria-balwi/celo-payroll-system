c45320cca20b7cb51f3cc92565c6cb0ef3f4caaa
maria balwi <patricereyes.MPR@gmail.com>
2024-09-02 17:55:35 +0800

    updated modal label on user portal: leaves and change shift modules

--- pages/user_changeShift.php
+++ pages/user_changeShift.php
@@ -104,7 +104,7 @@
                     <div class="modal-dialog modal-none modal-sm modal-dialog-centered">
                         <div class="modal-content" id="fileRequestModal">
                             <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="userFormLabel">New User</h1>
+                                <h1 class="modal-title fs-5" id="userFormLabel">File Change Shift Request</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
--- pages/user_leaves.php
+++ pages/user_leaves.php
@@ -118,7 +118,7 @@
                         <div class="modal-dialog modal-none modal-sm modal-dialog-centered">
                             <div class="modal-content" id="fileLeaveModal">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="userFormLabel">New User</h1>
+                                    <h1 class="modal-title fs-5" id="userFormLabel">File Leave</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
