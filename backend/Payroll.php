00b285652d0b37130bcc6a90470f87b588ac1330
maria balwi <patricereyes.MPR@gmail.com>
2024-10-23 15:13:02 +0800

    created new functions for adding, deleting, and updating adjustments in the admin portal - employees and adjustments modules

--- backend/Payroll.php
+++ backend/Payroll.php
@@ -17,7 +17,9 @@
         private $reimbursements = 'tbl_reimbursements';
         private $adjustments = 'tbl_adjustments';
         private $empAllowances = 'tbl_empallowances';
+        private $empReimbursements = 'tbl_empreimbursements';
         private $empDeductions = 'tbl_empdeductions';
+        private $empAdjustments = 'tbl_empadjustments';
         private $payroll = 'tbl_payroll';
         private $payrollCycle = 'tbl_payrollcycle';
         private $payslip = 'tbl_payslip';
@@ -115,12 +117,31 @@
             return $updateAllowance;
         }
 
+        public function updateReimbursement($reimbursementID, $reimbursementName) {
+            $updateReimbursement = "
+                UPDATE ".$this->reimbursements."
+                SET reimbursementName = '$reimbursementName'
+                WHERE reimbursementID = '$reimbursementID'";
+            return $updateReimbursement;
+        }
+
+
         public function updateDeduction($deductionID, $deductionName) {
             $updateDeduction = "
                 UPDATE ".$this->deductions."
                 SET deductionName = '$deductionName'
                 WHERE deductionID = '$deductionID'";
             return $updateDeduction;
+        }
+
+
+        public function updateAdjustment($adjustmentID, $adjustmentName, $adjustmentType) {
+            $updateAdjustment = "
+                UPDATE ".$this->adjustments."
+                SET adjustmentName = '$adjustmentName', 
+                adjustmentType = '$adjustmentType'
+                WHERE adjustmentID = '$adjustmentID'";
+            return $updateAdjustment;
         }
 
         public function deleteAllowance($allowanceID) {
@@ -130,11 +151,25 @@
             return $deleteAllowance;
         }
 
+        public function deleteReimbursement($reimbursementID) {
+            $deleteReimbursement = "
+                DELETE FROM ".$this->reimbursements."
+                WHERE reimbursementID = '$reimbursementID'";
+            return $deleteReimbursement;
+        }
+
         public function deleteDeduction($deductionID) {
             $deleteDeduction = "
                 DELETE FROM ".$this->deductions."
                 WHERE deductionID = '$deductionID'";
             return $deleteDeduction;
+        }
+
+        public function deleteAdjustment($adjustmentID) {
+            $deleteAdjustment = "
+                DELETE FROM ".$this->adjustments."
+                WHERE adjustmentID = '$adjustmentID'";
+            return $deleteAdjustment;
         }
 
         public function viewLastAllowance() {
@@ -186,6 +221,19 @@
             return $empAllowances;
         }
 
+        public function getAllEmpReimbursements($id) {
+            $empReimbursements = "
+                SELECT empReimbursements.empID, empReimbursements.reimbursementID, 
+                empReimbursementID, reimbursementName, amount
+                FROM ".$this->empReimbursements." AS empReimbursements
+                INNER JOIN ".$this->reimbursements." AS reimbursements
+                ON empReimbursements.reimbursementID = reimbursements.reimbursementID
+                INNER JOIN ".$this->employees." AS employees
+                ON empReimbursements.empID = employees.id
+                WHERE empReimbursements.empID = '$id'";
+            return $empReimbursements;
+        }
+
         public function getAllEmpADeductions($id) {
             $empAllowances = "
                 SELECT empDeductions.empID, empDeductions.deductionID, 
@@ -199,6 +247,19 @@
             return $empAllowances;
         }
 
+        public function getAllEmpAdjustments($id) {
+            $empAdjustments = "
+                SELECT empAdjustments.empID, empAdjustments.adjustmentID, 
+                empAdjustmentID, adjustmentName, amount, adjustmentType
+                FROM ".$this->empAdjustments." AS empAdjustments
+                INNER JOIN ".$this->adjustments." AS adjustments
+                ON empAdjustments.adjustmentID = adjustments.adjustmentID
+                INNER JOIN ".$this->employees." AS employees
+                ON empAdjustments.empID = employees.id
+                WHERE empAdjustments.empID = '$id'";
+            return $empAdjustments;
+        }
+
         public function checkEmpAllowance($id, $allowanceID) {
             $checkEmpAllowance = "
                 SELECT * FROM ".$this->empAllowances."
@@ -207,18 +268,12 @@
             return $checkEmpAllowance;
         }
 
-        public function addEmpAllowance($id, $allowanceID, $allowanceType, $amount) {
-            $addEmpAllowance = "
-                INSERT INTO ".$this->empAllowances." (empID, allowanceID, type, amount, dateCreated)
-                VALUES ('$id', '$allowanceID', '$allowanceType', '$amount', CURRENT_TIMESTAMP())";
-            return $addEmpAllowance;
-        }
-
-        public function addEmpAllowance_once($id, $allowanceID, $allowanceType, $amount, $effectiveDate) {
-            $addEmpAllowance = "
-                INSERT INTO ".$this->empAllowances." (empID, allowanceID, type, amount, effectiveDate, dateCreated)
-                VALUES ('$id', '$allowanceID', '$allowanceType', '$amount', '$effectiveDate', CURRENT_TIMESTAMP())";
-            return $addEmpAllowance;
+        public function checkEmpReimbursement($id, $reimbursementID) {
+            $checkEmpReimbursement = "
+                SELECT * FROM ".$this->empReimbursements."
+                WHERE empID = '$id'
+                AND reimbursementID = '$reimbursementID'";
+            return $checkEmpReimbursement;
         }
 
         public function checkEmpDeduction($id, $deductionID) {
@@ -229,11 +284,54 @@
             return $checkEmpDeduction;
         }
 
+        public function checkEmpAdjustment($id, $adjustmentID) {
+            $checkEmpAdjustment = "
+                SELECT * FROM ".$this->empAdjustments."
+                WHERE empID = '$id'
+                AND adjustmentID = '$adjustmentID'";
+            return $checkEmpAdjustment;
+        }
+
+        public function addEmpAllowance($id, $allowanceID, $allowanceType, $amount) {
+            $addEmpAllowance = "
+                INSERT INTO ".$this->empAllowances." (empID, allowanceID, type, amount, dateCreated)
+                VALUES ('$id', '$allowanceID', '$allowanceType', '$amount', CURRENT_TIMESTAMP())";
+            return $addEmpAllowance;
+        }
+
+        public function addEmpReimbursement($id, $reimbursementID, $reimbursementType, $amount) {
+            $addEmpReimbursement = "
+                INSERT INTO ".$this->empReimbursements." (empID, reimbursementID, type, amount, dateCreated)
+                VALUES ('$id', '$reimbursementID', '$reimbursementType', '$amount', CURRENT_TIMESTAMP())";
+            return $addEmpReimbursement;
+        }
+
         public function addEmpDeduction($id, $deductionID, $deductionType, $amount) {
             $addEmpADeduction = "
                 INSERT INTO ".$this->empDeductions." (empID, deductionID, type, amount, dateCreated)
                 VALUES ('$id', '$deductionID', '$deductionType', '$amount', CURRENT_TIMESTAMP())";
             return $addEmpADeduction;
+        }
+
+        public function addEmpAdjustment($id, $adjustmentID, $adjustmentType, $amount) {
+            $addEmpAdjustment = "
+                INSERT INTO ".$this->empAdjustments." (empID, adjustmentID, type, amount, dateCreated)
+                VALUES ('$id', '$adjustmentID', '$adjustmentType', '$amount', CURRENT_TIMESTAMP())";
+            return $addEmpAdjustment;
+        }
+
+        public function addEmpAllowance_once($id, $allowanceID, $allowanceType, $amount, $effectiveDate) {
+            $addEmpAllowance = "
+                INSERT INTO ".$this->empAllowances." (empID, allowanceID, type, amount, effectiveDate, dateCreated)
+                VALUES ('$id', '$allowanceID', '$allowanceType', '$amount', '$effectiveDate', CURRENT_TIMESTAMP())";
+            return $addEmpAllowance;
+        }
+
+        public function addEmpReimbursement_once($id, $reimbursementID, $reimbursementType, $amount, $effectiveDate) {
+            $addEmpReimbursement = "
+                INSERT INTO ".$this->empReimbursements." (empID, reimbursementID, type, amount, effectiveDate, dateCreated)
+                VALUES ('$id', '$reimbursementID', '$reimbursementType', '$amount', '$effectiveDate', CURRENT_TIMESTAMP())";
+            return $addEmpReimbursement;
         }
 
         public function addEmpDeduction_once($id, $deductionID, $deductionType, $amount, $effectiveDate) {
@@ -243,6 +341,13 @@
             return $addEmpADeduction;
         }
 
+        public function addEmpAdjustment_once($id, $adjustmentID, $adjustmentType, $amount, $effectiveDate) {
+            $addEmpAdjustment = "
+                INSERT INTO ".$this->empAdjustments." (empID, adjustmentID, type, amount, effectiveDate, dateCreated)
+                VALUES ('$id', '$adjustmentID', '$adjustmentType', '$amount', '$effectiveDate', CURRENT_TIMESTAMP())";
+            return $addEmpAdjustment;
+        }
+
         public function deleteEmpAllowance($empAllowanceID) {
             $deleteEmpAllowance = "
                 DELETE FROM ".$this->empAllowances."
@@ -250,11 +355,25 @@
             return $deleteEmpAllowance;
         }
 
-        public function deleteEmpADeduction($empDeductionID) {
+        public function deleteEmpReimbursement($empReimbursementID) {
+            $deleteEmpReimbursement = "
+                DELETE FROM ".$this->empReimbursements."
+                WHERE empReimbursementID = '$empReimbursementID'";
+            return $deleteEmpReimbursement;
+        }
+
+        public function deleteEmpDeduction($empDeductionID) {
             $deleteEmpADeduction = "
                 DELETE FROM ".$this->empDeductions."
                 WHERE empDeductionID = '$empDeductionID'";
             return $deleteEmpADeduction;
+        }
+
+        public function deleteEmpAdjustment($empAdjustmentID) {
+            $deleteEmpAdjustment = "
+                DELETE FROM ".$this->empAdjustments."
+                WHERE empAdjustmentID = '$empAdjustmentID'";
+            return $deleteEmpAdjustment;
         }
 
         public function viewAllPayroll() {
