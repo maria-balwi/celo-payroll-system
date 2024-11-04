ef165b84e891d1246dcecfd320a8cc5410fd5996
maria balwi <patricereyes.MPR@gmail.com>
2024-11-04 20:08:56 +0800

    created function to update holiday in admin portal: holidays module

---
+++ backend/admin/updateHoliday.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include '../../init.php';
+    $conn = $database->dbConnect();
+    session_start();
+
+    $updateHolidayID = $_POST['updateHolidayID'];
+    $updateHolidayName = $_POST['updateHolidayName'];
+    $updateDateFrom = $_POST['updateDateFrom'];
+    $updateDateTo = $_POST['updateDateTo'];
+    $updateHolidayType = $_POST['updateHolidayType'];
+
+    mysqli_query($conn, $payroll->updateHoliday($updateHolidayID, $updateHolidayName, $updateDateFrom, $updateDateTo, $updateHolidayType));
+    
+    // ERROR MESSAGE
+    $em = "Holiday Information Updated Successfully";
+    // RESPONSE ARRAY
+    $error = array('error' => 0, 'em' => $em);
+    echo json_encode($error);
+    exit();
+?>
