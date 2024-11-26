b170dcb80a3a6728891a3ab82e034ede2a88378d
maria balwi <patricereyes.MPR@gmail.com>
2024-11-26 10:17:56 +0800

    removed usage of mobile device

--- index.php
+++ index.php
@@ -28,13 +28,6 @@
                     <label for="password" class="block text-gray-700 mb-2">Password</label>
                     <input type="password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                 </div>
-                <!-- <div class="flex items-center justify-between mb-4">
-                    <div class="flex items-center">
-                        <input type="checkbox" id="remember" name="remember" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded">
-                        <label for="remember" class="ml-2 block text-gray-900">Remember me</label>
-                    </div>
-                    <a href="#" class="text-sm text-blue-500 hover:underline">Forgot password?</a>
-                </div> -->
                 <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600" id="btnLogin">Login</button>
             </form>
         </div>
