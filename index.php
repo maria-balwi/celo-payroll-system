c852d54e60efa7efe879ff5e19e15f1f9a93b21b
maria balwi <patricereyes.MPR@gmail.com>
2024-12-06 00:23:17 +0800

    auto focus on email when login

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <form autocomplete="on" id="loginForm" class="form-container">
                 <div class="mb-4">
                     <label for="username" class="block text-gray-700 mb-2">Username</label>
-                    <input type="text" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <input type="text" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" autofocus>
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block text-gray-700 mb-2">Password</label>
