cc222c656309a903bd2af567875f158c4dd33a57
maria-balwi <patricereyes.MPR@gmail.com>
2025-11-05 13:54:02 +0800

    updated config file

--- config/config.php
+++ config/config.php
@@ -9,3 +9,15 @@
     //API Keys
 
 ?>  
+
+<?php
+
+    // //DATABASE CONFIGURATIONS
+    // $host = "localhost";
+    // $username = "celobusi_payroll_user";
+    // $password = "C3l0p@ssw0rd@65";
+    // $database = "celobusi_payroll";
+
+    // //API Keys
+
+?>  
