9e2641ad631c398d388951ec66d67b194509a9d4
maria-balwi <patricereyes.MPR@gmail.com>
2025-11-20 16:04:40 +0800

    changed db configurations for deployment

--- config/config.php
+++ config/config.php
@@ -1,11 +1,10 @@
 <?php
-    // // DATABASE CONFIGURATIONS
+    // //DATABASE CONFIGURATIONS
     // $host = "localhost";
-    // $username = "celobusi_system_user";
     // $username = "celobusi_system_user";
     // $password = "C3l0p@ssw0rd@65";
     // // $database = "payroll_db";
-    // $database = "celobusi_payroll_system";
+    // $database = "celobusi_payroll_system_system";
 
     // // API Keys
     // // SENTRY API CONFIGURATION
@@ -26,4 +25,4 @@
     define('SENTRY_BASE_URL', 'https://avid-snowsuit-hastily.ngrok-free.dev');
     define('SENTRY_API_USERNAME', '1010');
     define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
-?> 
+?>  
