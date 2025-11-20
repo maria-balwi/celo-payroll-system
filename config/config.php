c9006e9567da5323dcc073f73d580acea69f2832
maria-balwi <patricereyes.MPR@gmail.com>
2025-11-20 16:04:40 +0800

    changed db configurations for deployment

--- config/config.php
+++ config/config.php
@@ -1,23 +1,23 @@
 <?php
 
-    //DATABASE CONFIGURATIONS
-    $host = "localhost";
-    $username = "celobusi_system_user";
-    $password = "C3l0p@ssw0rd@65";
-    $database = "celobusi_payroll_system";
+    // //DATABASE CONFIGURATIONS
+    // $host = "localhost";
+    // $username = "celobusi_system_user";
+    // $password = "C3l0p@ssw0rd@65";
+    // $database = "celobusi_payroll_system";
 
-    //API Keys
+    // //API Keys
 
 ?>  
 
 <?php
 
-    // //DATABASE CONFIGURATIONS
-    // $host = "localhost";
-    // $username = "celobusi_payroll_user";
-    // $password = "C3l0p@ssw0rd@65";
-    // $database = "celobusi_payroll";
+    //DATABASE CONFIGURATIONS
+    $host = "localhost";
+    $username = "celobusi_payroll_user";
+    $password = "C3l0p@ssw0rd@65";
+    $database = "celobusi_payroll";
 
-    // //API Keys
+    //API Keys
 
 ?>  
