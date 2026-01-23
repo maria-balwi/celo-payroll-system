09c25f45f26127fa2876c154605462c6a9b0e410
maria-balwi <patricereyes.MPR@gmail.com>
2026-01-23 15:47:59 +0800

    changed database configurations bsed on a2hosting configuration

--- config/config.php
+++ config/config.php
@@ -1,18 +1,5 @@
 <?php
-
-    //DATABASE CONFIGURATIONS
-    $host = "localhost";
-    $username = "celobusi_system_user";
-    $password = "C3l0p@ssw0rd@65";
-    $database = "celobusi_payroll_system";
-
-    //API Keys
-
-?>  
-
-<?php
-
-    // //DATABASE CONFIGURATIONS
+    // // DATABASE CONFIGURATIONS
     // $host = "localhost";
     // $username = "celobusi_system_user";
     // $password = "C3l0p@ssw0rd@65";
