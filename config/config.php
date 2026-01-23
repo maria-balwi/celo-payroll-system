d1d510912f60a93a8981ca73d2aaa7fdf2cfa671
maria-balwi <patricereyes.MPR@gmail.com>
2026-01-23 15:44:57 +0800

    changed database configurations bsed on a2hosting configuration

--- config/config.php
+++ config/config.php
@@ -1,27 +1,27 @@
 <?php
-    // DATABASE CONFIGURATIONS
-    $host = "localhost";
-    $username = "celobusi_system_user";
-    $password = "C3l0p@ssw0rd@65";
-    $database = "celobusi_payroll_system";
-
-    // API Keys
-    // SENTRY API CONFIGURATION
-    define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
-    define('SENTRY_API_USERNAME', '1010');
-    define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
-?>  
-
-<?php
     // // DATABASE CONFIGURATIONS
     // $host = "localhost";
-    // $username = "celobusi_payroll_user";
+    // $username = "celobusi_system_user";
     // $password = "C3l0p@ssw0rd@65";
-    // $database = "celobusi_payroll";
+    // $database = "celobusi_payroll_system";
 
     // // API Keys
     // // SENTRY API CONFIGURATION
     // define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
     // define('SENTRY_API_USERNAME', '1010');
     // define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
+?>  
+
+<?php
+    // DATABASE CONFIGURATIONS
+    $host = "localhost";
+    $username = "celobusi_payroll_user";
+    $password = "C3l0p@ssw0rd@65";
+    $database = "celobusi_payroll";
+
+    // API Keys
+    // SENTRY API CONFIGURATION
+    define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
+    define('SENTRY_API_USERNAME', '1010');
+    define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
