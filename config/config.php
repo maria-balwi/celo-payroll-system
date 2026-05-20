2d0fb756d04d9d7c75e422874d32448cbbad7be5
maria balwi <patricereyes.MPR@gmail.com>
2026-05-21 07:53:56 +0800

    fix: updated sentry link for fetching attendance logs from sentry

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@
 
     // API Keys
     // SENTRY API CONFIGURATION
-    define('SENTRY_BASE_URL', 'https://gusty-overfoolish-aydan.ngrok-free.dev/api/Integration');
+    define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
     define('SENTRY_API_USERNAME', '1010');
     define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
@@ -22,7 +22,7 @@
 
     // // API Keys
     // // SENTRY API CONFIGURATION
-    // define('SENTRY_BASE_URL', 'https://gusty-overfoolish-aydan.ngrok-free.dev/api/Integration');
+    // define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
     // define('SENTRY_API_USERNAME', '1010');
     // define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
