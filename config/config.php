0083ee370ad93b3467ead4169108095ddd995a7c
maria balwi <patricereyes.MPR@gmail.com>
2026-05-21 08:29:31 +0800

    fix: updates sentry link

--- config/config.php
+++ config/config.php
@@ -1,15 +1,14 @@
 <?php
-    // //DATABASE CONFIGURATIONS
     // // DATABASE CONFIGURATIONS
     // $host = "localhost";
     // $username = "celobusi_system_user";
     // $password = "C3l0p@ssw0rd@65";
     // // $database = "payroll_db";
-    // $database = "celobusi_payroll_system_system";
+    // $database = "celobusi_payroll_system";
 
     // // API Keys
     // // SENTRY API CONFIGURATION
-    // define('SENTRY_BASE_URL', 'https://avid-snowsuit-hastily.ngrok-free.dev');
+    // define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
     // define('SENTRY_API_USERNAME', '1010');
     // define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
@@ -23,7 +22,7 @@
 
     // API Keys
     // SENTRY API CONFIGURATION
-    define('SENTRY_BASE_URL', 'https://avid-snowsuit-hastily.ngrok-free.dev');
+    define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
     define('SENTRY_API_USERNAME', '1010');
     define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
