5416176808ad46d411956b7492a283cf3af5cb49
maria-balwi <patricereyes.MPR@gmail.com>
2026-06-03 16:55:03 +0800

    fix: updated sentry link

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 
     // // API Keys
     // // SENTRY API CONFIGURATION
-    // define('SENTRY_BASE_URL', 'https://gusty-overfoolish-aydan.ngrok-free.dev/api/Integration');
+    // define('SENTRY_BASE_URL', 'https://avid-snowsuit-hastily.ngrok-free.dev');
     // define('SENTRY_API_USERNAME', '1010');
     // define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
@@ -23,7 +23,7 @@
 
     // API Keys
     // SENTRY API CONFIGURATION
-    define('SENTRY_BASE_URL', 'https://nonfimbriated-norris-first.ngrok-free.dev/api/Integration');
+    define('SENTRY_BASE_URL', 'https://avid-snowsuit-hastily.ngrok-free.dev');
     define('SENTRY_API_USERNAME', '1010');
     define('SENTRY_API_PASSWORD', 'C3l0p@ssw0rd@65');
 ?>  
